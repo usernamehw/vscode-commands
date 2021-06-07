5b45437e13b96b227354578fc9342f7a0981e3d2
al <usernamehw@gmail.com>
2021-06-07 17:07:52 +0300

    Fix `populateCommandPalette` setting duplicating commands

--- src/commandPalette.ts
+++ src/commandPalette.ts
@@ -10,38 +10,52 @@ interface ICommand {
 	category?: string;
 }
 
+const coreCommandIds = [
+	'commands.openAsQuickPick',
+	'commands.suggestCommands',
+	'commands.newCommand',
+	'commands.newCommandInFolder',
+	'commands.revealCommand',
+	'commands.assignKeybinding',
+	'commands.addToStatusBar',
+	'commands.revealCommandsInSettignsGUI',
+];
+/**
+ * VSCode doesn't support dynamic Command Palette items: https://github.com/microsoft/vscode/issues/1422
+ *
+ * This function updates `package.json` file to add items from `commands.commands` to Command Palette (but requires editor reload after changing configuration)
+ */
 export async function updateCommandPalette(items: TopLevelCommands, context: ExtensionContext) {
 	if (!extensionConfig.populateCommandPalette) {
 		return;
 	}
-	const packageJsonPath = context.asAbsolutePath('./package.json');
-	const packageJson = await fs.promises.readFile(packageJsonPath);
-	const packageAsJSON = JSON.parse(packageJson.toString());
-	const oldCommands: ICommand[] = packageAsJSON.contributes.commands;
-	const newCommands: ICommand[] = [];
 
-	const additionalCommands: ICommand[] = [];
+	const packageJsonPath = context.asAbsolutePath('./package.json');
+	const packageJsonFile = await fs.promises.readFile(packageJsonPath);
+	const packageJSONObject = JSON.parse(packageJsonFile.toString());
+	const oldCommands = packageJSONObject.contributes.commands as ICommand[];
+	const coreCommands: ICommand[] = (packageJSONObject.contributes.commands as ICommand[]).filter(command => coreCommandIds.includes(command.command));
+
+	const userCommands: ICommand[] = [];
 	forEachItem((item, key) => {
 		if (item.nestedItems) {
-			return;
+			return;// Skip folders
 		}
-		additionalCommands.push({
+		userCommands.push({
 			command: key,
 			title: key,
 			category: 'Commands',
 		});
 	}, items);
-	for (const newCom of additionalCommands) {
-		if (oldCommands.find(oldCom => oldCom.command === newCom.command && oldCom.title === newCom.title)) {
-			continue;
-		}
-		newCommands.push(newCom);
+	const newCommands = [...coreCommands, ...userCommands];
+
+	if (JSON.stringify(newCommands.sort((a, b) => a.command.localeCompare(b.command))) ===
+		JSON.stringify(oldCommands.sort((a, b) => a.command.localeCompare(b.command)))) {
+		return;// Only write file if necessary
 	}
-	if (newCommands.length === 0) {
-		return;
-	}
-	packageAsJSON.contributes.commands = [...oldCommands, ...newCommands];
-	await fs.promises.writeFile(packageJsonPath, JSON.stringify(packageAsJSON, null, '\t'));
+
+	packageJSONObject.contributes.commands = [...coreCommands, ...userCommands];
+	await fs.promises.writeFile(packageJsonPath, JSON.stringify(packageJSONObject, null, '\t'));
 }
 
 /**
