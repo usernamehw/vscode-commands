f5e81ac5d556af101d975915926e60ae10594c32
al <usernamehw@gmail.com>
2021-06-30 11:50:33 +0300

    💄

--- src/registerUserCommands.ts
+++ src/registerUserCommands.ts
@@ -1,9 +1,8 @@
 import { commands } from 'vscode';
-import { getAllVscodeCommands } from './commands';
 import { registeredCommandsList } from './extension';
 import { run } from './run';
 import { TopLevelCommands } from './types';
-import { forEachItem } from './utils';
+import { forEachItem, getAllVscodeCommands } from './utils';
 
 /**
  * Register a command to be able to execute it from a keybinding.
