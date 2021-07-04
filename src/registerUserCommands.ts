0ae1d9a913ca4064bba4e16d811437f71d8c8005
al <usernamehw@gmail.com>
2021-07-04 21:02:38 +0300

    Register all aliases

--- src/registerUserCommands.ts
+++ src/registerUserCommands.ts
@@ -1,4 +1,5 @@
 import { commands, Disposable } from 'vscode';
+import { extensionConfig } from './extension';
 import { run } from './run';
 import { TopLevelCommands } from './types';
 import { forEachItem, getAllVscodeCommands } from './utils';
@@ -22,6 +23,16 @@ export async function updateUserCommands(items: TopLevelCommands) {
 			run(item);
 		}));
 	}, items);
+
+	for (const alias in extensionConfig.alias) {
+		const command = extensionConfig.alias[alias];
+		registeredCommandsList.push(commands.registerCommand(alias, (args: unknown) => {
+			run({
+				command,
+				args,
+			});
+		}));
+	}
 }
 /**
  * Dispose user defined commands.
