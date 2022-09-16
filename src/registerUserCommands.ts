606b1dda5df15a4a6912de295a9ec09ba50ec61b
al <usernamehw@gmail.com>
2022-09-16 20:27:20 +0300

    fix: show error notification when command or alias failed to register

--- src/registerUserCommands.ts
+++ src/registerUserCommands.ts
@@ -2,49 +2,50 @@ import { commands, Disposable, window } from 'vscode';
 import { $config } from './extension';
 import { run } from './run';
 import { TopLevelCommands } from './types';
-import { forEachCommand, getAllVscodeCommands, goToSymbol, openSettingsJSON } from './utils';
+import { forEachCommand } from './utils';
 
-const registeredCommandsList: Disposable[] = [];
+const registeredCommandsDisposables: Disposable[] = [];
 
 /**
- * Register a command to be able to execute it from a keybinding.
+ * Register commands to be able to execute it from a keybinding.
  */
-export async function updateUserCommands(items: TopLevelCommands): Promise<void> {
+export function updateUserCommands(items: TopLevelCommands): void {
 	unregisterUserCommands();
 
-	const allCommands = await getAllVscodeCommands();
-
-	forEachCommand(async (item, key) => {
-		if (allCommands.includes(key)) {
-			const revealButton = 'Reveal';
-			const button = await window.showWarningMessage(`Command key must be unique (even among already registered commands) "${key}".`, revealButton);
-			if (button) {
-				await openSettingsJSON(item.workspace ? 'workspace' : 'global');
-				await goToSymbol(window.activeTextEditor, key);
-			}
-			return;
+	forEachCommand((item, key) => {
+		let disposable: Disposable;
+		try {
+			disposable = commands.registerCommand(key, () => {
+				run(item);
+			});
+			registeredCommandsDisposables.push(disposable);
+		} catch (err) {
+			window.showErrorMessage(`Failed to register command: ${(err as Error).message}`);
 		}
-		registeredCommandsList.push(commands.registerCommand(key, () => {
-			run(item);
-		}));
 	}, items);
 
 	for (const alias in $config.alias) {
 		const command = $config.alias[alias];
-		registeredCommandsList.push(commands.registerCommand(alias, (args: unknown) => {
-			run({
-				command,
-				args,
+		let disposable: Disposable;
+		try {
+			disposable = commands.registerCommand(alias, (args: unknown[]) => {
+				run({
+					command,
+					args,
+				});
 			});
-		}));
+			registeredCommandsDisposables.push(disposable);
+		} catch (err) {
+			window.showErrorMessage(`Failed to register alias: ${(err as Error).message}`);
+		}
 	}
 }
 /**
  * Dispose user defined commands.
  */
 export function unregisterUserCommands() {
-	for (const command of registeredCommandsList) {
+	for (const command of registeredCommandsDisposables) {
 		command.dispose();
 	}
-	registeredCommandsList.length = 0;
+	registeredCommandsDisposables.length = 0;
 }
